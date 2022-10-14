dfadc1a7ab819c43beda6eff8f4ed91eb6d90990
trunghi1412 <91446424+trunghi1412@users.noreply.github.com>
2022-10-14 09:47:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express()
 const port = 5000;
+mongoose.connect(
+    `mongodb+srv://Hieu1412:hieudepzai1412@cluster0.9nlbp6d.mongodb.net/test`,
+    {
+      useNewUrlParser: true,
+     
+      useUnifiedTopology: true
+    }
+  );
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error: "));
+  db.once("open", function () {
+    console.log("Connected successfully");
+  });
 
 app.get("/users", (req,res) => {
   res.send("Hieu dep trai hoc IT");
